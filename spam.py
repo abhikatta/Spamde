790f0cd063ef5ced95e5bd2b244f2f34cf69c4be
Caesar01108 <76813100+Caesar01108@users.noreply.github.com>
2021-08-09 13:03:48 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,16 +1,8 @@
 import pyautogui, time
 time.sleep(2)
-f = open ("D:\VS Code\script",'r')
+f = open ("path",'r') #add the path to the script file here. script file is a txt file  
 d = open ("script",'r')
 for word in d:
     pyautogui.typewrite(word)
     pyautogui.press("enter")  
-
-Method-2 :
-+++++++++++++
-msg = input("Enter your message:")
-n = input("How many times?: ")
-time.sleep(2)
-for i in range(0,int(n)):
-    pyautogui.typewrite(msg + '\n')
-     
+ 
