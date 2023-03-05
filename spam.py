7c836ad3b2be42228b09f46abd6ff26b68ae6bd6
Abhi Katta <Abhinay Katta>
2023-03-05 23:12:09 +0530

    'asdsa'

--- spam.py
+++ spam.py
@@ -1,7 +1,15 @@
-import pyautogui, time
-time.sleep(2)
-f = open ("path",'r') #add the path to the script file here. script file is a txt file  
-d = open ("script",'r')
-for word in d:
-    pyautogui.typewrite(word)
-    pyautogui.press("enter")
+import pyautogui
+import time
+import sys
+sys.path.append('./')
+
+
+def spam_from_msg():
+    time.sleep(2)
+    # add the path to the script file here. script file is a txt file
+    f = open("path", 'r')
+    d = open("script", 'r')
+    for word in d:
+        pyautogui.typewrite(word)
+
+        pyautogui.press("enter")
