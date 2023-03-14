378a0692383d8c490e6be3be3fb4ad79272c86e0
Abhi Katta <Abhinay Katta>
2023-03-14 15:08:35 +0530

    file-input

--- spam.py
+++ spam.py
@@ -6,28 +6,48 @@ import sys
 sys.path.append('./')
 
 
-def spam_random_string(spam_count):
+def spam(msg, spam_count):
     print("you have 3 seconds")
     time.sleep(3)
-    while (spam_count > -1):
-        size = random.randint(1, 20)
-        msg = (''.join(random.choices(string.ascii_letters, k=size))).lower()
+    while (spam_count > 0):
         pyautogui.typewrite(msg)
         pyautogui.press("enter")
         spam_count -= 1
 
 
-def take_input_from_cli():
-    spam_count = int(input("enter spam count:"))
-    spam_random_string(spam_count)
+def take_input():
+    instr = "0 for msg and spam_count\n1 for random strings and spam_count\n2 for file and spam_count\n3 to quit"
+    print("Instructions:\n")
+    print(instr)
+    print("\n")
+    inputs = input("Enter 0 or 1 or 2 or 3:\n")
+    if (inputs == "0"):
+        msg = input("\nEnter msg you want to spam:\n")
+        spam_count = int(input("\nEnter how many times you want to spam:\n"))
+        spam(msg, spam_count)
+    elif (inputs == "1"):
+        spam_count = int(input("\nEnter how many times you want to spam:\n"))
+        print("you have 3 seconds")
+        time.sleep(3)
+        while (spam_count > 0):
+            size = random.randint(1, 20)
+            msg = (''.join(random.choices(string.ascii_letters, k=size))).lower()
+            pyautogui.typewrite(msg)
+            pyautogui.press("enter")
+            spam_count -= 1
+
+    elif (inputs == "2"):
+        spam_file = input("\nThrow in the address of the file:\n")
+        spam_count = int(input("\nEnter how many times you want to spam:\n"))
+        open_spam_file = open(spam_file, 'r')
+        for i in open_spam_file:
+            pyautogui.typewrite(i)
+            pyautogui.press("enter")
+    elif (inputs == "3"):
+        quit
+    else:
+        print("Here are the instructions again,read them carefully:")
+        take_input()
 
 
-take_input_from_cli()
-
-
-''' 
-TODO:
-1. Fix everything 
-2. Build GUI
-3. Complete the project.
-'''
+take_input()
