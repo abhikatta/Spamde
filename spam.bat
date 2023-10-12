88bcca2128ec22e7b9ccd4e92ca69350c4d6e3e2
Abhi Katta <abhinaykatta97@gmail.com>
2023-10-12 09:04:13 +0530

    fixed install python from store

--- spam.bat
+++ spam.bat
@@ -1,1 +1,4 @@
-@py.exe "%~dp0spam.py" %*
+@REM if you installed python from store uncomment this line:
+@python3.exe "%~dp0spam.py" %*
+@REM if you installed python from www.python.org uncomment this line:
+@pyc "%~dp0spam.py" %*
